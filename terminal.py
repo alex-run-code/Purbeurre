5e8d1030da52895e902822a2597c3fc852920040
megalex97 <megalex97@hotmail.com>
2019-04-12 16:08:04 +0300

    can display nutriscore of selected food

--- terminal.py
+++ terminal.py
@@ -17,7 +17,7 @@ def display_terminal():
     choice = input ("Selectionnez un menu \n"
     "1- Voir la liste des catégories \n"
     "2- Voir mes aliments substitués \n"
-    "\n Votre choix: \n")
+    "\nVotre choix: ")
     if choice == "1":
         display_categories()
     else: 
@@ -26,23 +26,30 @@ def display_terminal():
 # This function display the categories
 def display_categories():
     mycursor.execute("SHOW TABLES")
-    categories = mycursor.fetchall() 
-    liste_categories = []
-    for x in categories:
-        liste_categories.append(x)     # for some reason this doesnt simply add the table " boissons " but a tuple: " ('boisson',) "
-    for item in enumerate(liste_categories):
-        print(item)
-    choice = input("Selectionnez une catégorie\n Votre choix: ")
-    sql = " SELECT Product_name AS Aliment FROM " + liste_categories[int(choice)][0]
+    categories = mycursor.fetchall()     # for some reason this doesnt simply add the table " boissons " but a tuple: " ('boisson',) "
+    for i, item in enumerate(categories):
+        print(i+1,"-",item[0])
+    choice = input("Selectionnez une catégorie\n \nVotre choix: ")
+    choice = int(choice) - 1
+    sql = " SELECT Product_name AS Aliment FROM " + categories[choice][0]
     mycursor.execute(sql)
     product_names = mycursor.fetchall()
-    for y in (product_names):
-        print(y)                   # meme probleme que plus haut, ca renvoie des tuples et non simplement le nom des aliments
+    for i, item in enumerate(product_names):
+        print(i+1, "-", item[0])           # meme probleme que plus haut, ca renvoie des tuples et non simplement le nom des aliments
+    selected_food = input("Selectionnez un aliment à substituer\n \nVotre choix: ")
+    selected_food = int(selected_food)
+    find_nutriscore = "SELECT nova_group FROM " + categories[choice][0] + " WHERE id =" + str(selected_food)
+    mycursor.execute(find_nutriscore)
+    nutriscore = mycursor.fetchall()
+    print(nutriscore)
+    #find_substitute = "SELECT * FROM " + categories[choice][0] + " WHERE nova_group >" + find_nutriscore + "ORDER BY RAND() LIMIT 1"
+    #mycursor.execute(find_nutriscore)
 
 
 
-        
 
 
 
 
+
+display_terminal()
