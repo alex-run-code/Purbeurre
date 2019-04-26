fb0df3030de070880ddca0e348b759c59b712999
megalex97 <megalex97@hotmail.com>
2019-04-26 18:20:02 +0300

    updated database with foods_stores and stores

--- dbfiller.py
+++ dbfiller.py
@@ -20,28 +20,48 @@ def add_food_in_db(category):
     for food in products:
         rules = ['product_name_fr' in food,
                 'nova_group' in food,
-                'countries_tags' in food,
-                'stores_tags' in food,
                 'url' in food, 
+                'stores_tags' in food,
                 ]
         if not all(rules):
             continue
         if len(food['product_name_fr']) == 0:
             continue
-        Product_name_fr = str(food['product_name_fr'])
+        product_name_fr = str(food['product_name_fr'])
         nova_group = str(food['nova_group'])
-        countries_tags = str(food['countries_tags'])
-        stores_tags = str(food['stores_tags'])
         url = str(food['url'])
-        add_food_sql = "INSERT INTO foods (Product_name, nova_group, category_name, countries_tags, stores_tags, off_url) VALUES (%s, %s, %s, %s, %s, %s)"
-        val_food_sql = [(Product_name_fr, nova_group, category, countries_tags, stores_tags, url)]
+        stores_tags = food['stores_tags']
+        #adding store in stores
+        for store in stores_tags:
+            add_store_sql = "INSERT IGNORE INTO stores (stores_name) values (%s)"
+            val_store_sql = [(store,)]
+            mycursor.executemany(add_store_sql, val_store_sql)
+            mydb.commit()
+        #adding food in foods
+        mycursor.execute("SELECT id FROM categories WHERE category_name = '" + category + "'")
+        category_id = mycursor.fetchall()
+        category_id = category_id[0][0]
+        add_food_sql = "INSERT IGNORE INTO foods (product_name, nova_group, category_id, off_url) VALUES (%s, %s, %s, %s)"
+        val_food_sql = [(product_name_fr, nova_group, category_id, url)]
         mycursor.executemany(add_food_sql, val_food_sql)
         mydb.commit()
+        #adding foods id and stores id in foods stores
+        for store in stores_tags:
+            mycursor.execute('SELECT id FROM foods WHERE product_name = "' + product_name_fr + '"')
+            foods_id = mycursor.fetchall()
+            foods_id = int(foods_id[0][0])
+            mycursor.execute("SELECT id FROM stores WHERE stores_name = '" + store + "'")
+            stores_id = mycursor.fetchall()
+            stores_id = int(stores_id[0][0])
+            add_foods_id_sql = "INSERT INTO foods_stores (foods_id, stores_id) VALUES (%s, %s)"
+            val_foods_id_sql = [(foods_id, stores_id)]
+            mycursor.executemany(add_foods_id_sql, val_foods_id_sql)
+            mydb.commit()
         print("added successfully")
 
 # This function add a category in the database
 def add_category_in_db(category):
-    add_category_sql = 'INSERT INTO categories (category_name) VALUES (%s)'
+    add_category_sql = 'INSERT IGNORE INTO categories (category_name) VALUES (%s)'
     val_cat_sql = [(category,)]
     mycursor.executemany(add_category_sql, val_cat_sql)
     mydb.commit()
@@ -62,8 +82,9 @@ def fill_foods():
     add_food_in_db('viandes')
     add_food_in_db('charcuteries')
 
+
+
+
+fill_categories()
 fill_foods()
-
-
-
 
--- purbeurredb.sql
+++ purbeurredb.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE PurBeurreDb;
 
 CREATE TABLE categories(
-    id SMALLINT NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     category_name VARCHAR(250) UNIQUE NOT NULL,
     PRIMARY KEY (id)
 )
@@ -10,33 +10,70 @@ ENGINE = INNODB;
 ALTER TABLE categories
 ADD INDEX index_categ (category_name);
 
+
 CREATE TABLE foods(
-    id SMALLINT NOT NULL AUTO_INCREMENT,
-    Product_name VARCHAR(250) NOT NULL,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(250) UNIQUE NOT NULL, 
     nova_group INT(1) NOT NULL,
-    category_name VARCHAR(250) NOT NULL,
-    countries_tags TEXT(1000) NOT NULL,
-    stores_tags TEXT(1000),
-    off_url TEXT(1000),
+    category_id INT(250) UNSIGNED NOT NULL,
+    off_url VARCHAR(250),
     PRIMARY KEY (id)
 )
 ENGINE = INNODB;
 
 ALTER TABLE foods
-ADD FOREIGN KEY (category_name) REFERENCES categories(category_name);
+ADD INDEX index_foods_id (id),
+ADD FOREIGN KEY (category_id) REFERENCES categories(id);
+
 
 CREATE TABLE favorites(
-    id SMALLINT NOT NULL AUTO_INCREMENT,
-    Product_name VARCHAR(250) NOT NULL,
+    id INT(250) UNSIGNED NOT NULL AUTO_INCREMENT,
+    foods_subs_id INT(250) UNSIGNED UNIQUE NOT NULL,
+    foods_id INT(250) UNSIGNED UNIQUE NOT NULL,
     PRIMARY KEY (id)
 )
 ENGINE = INNODB;
+
+ALTER TABLE favorites
+ADD FOREIGN KEY (foods_subs_id) REFERENCES foods(id),
+ADD FOREIGN KEY (foods_id) REFERENCES foods(id);
+
+
+CREATE TABLE stores(
+    id INT(250) UNSIGNED NOT NULL AUTO_INCREMENT,
+    stores_name VARCHAR (250) UNIQUE,
+    PRIMARY KEY (id)
+)
+ENGINE = INNODB;
+
+ALTER TABLE stores
+ADD INDEX index_stores (id);
+
+
+CREATE TABLE foods_stores(
+    id INT(250) UNSIGNED AUTO_INCREMENT,
+    foods_id INT(250) UNSIGNED NULL,
+    stores_id INT(250) UNSIGNED NULL,
+    PRIMARY KEY (id)
+)
+ENGINE = INNODB;
+
+ALTER TABLE foods_stores
+ADD FOREIGN KEY (foods_id) REFERENCES foods(id),
+ADD FOREIGN KEY (stores_id) REFERENCES stores(id);
+
+
+
+
+
 
 -- commandes utiles 
 
 DROP TABLE foods;
 DROP TABLE categories;
 DROP TABLE favorites;
+DROP TABLE stores;
+DROP TABLE foods_stores;
 
 SELECT * FROM foods;
 SELECT * FROM categories;
@@ -47,3 +84,4 @@ DROP column energy;
 
 
 
+ALTER TABLE foods_stores MODIFY stores_id INT(250) UNSIGNED NULL;
